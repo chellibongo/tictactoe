941574ba0343a12957c460ef0b4a7e05ebf36472
Chris Ellis <ce8958@gmail.com>
2022-07-02 16:18:22 +0100

    chore: begin refactor so Game no longer calls Scanner directly

--- src/main/java/com/tictactoe/Input.java
+++ src/main/java/com/tictactoe/Input.java
@@ -4,4 +4,13 @@ import java.util.Scanner;
 
 public class Input {
     static final Scanner scan = new Scanner(System.in);
+    /*
+    TODO add exception handling and refactor Game to use theser
+    */
+    public static int getInt() {
+        return scan.nextInt();
+    }
+    public static String getString() {
+        return scan.nextLine();
+    }
 }
