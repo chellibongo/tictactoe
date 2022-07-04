9555170395494ce906b4ffaf5148770443a67a4a
Chris Ellis <ce8958@gmail.com>
2022-07-04 21:49:58 +0100

    refactor: changed calls in Main to use new methods in Input

--- src/main/java/com/tictactoe/Main.java
+++ src/main/java/com/tictactoe/Main.java
@@ -5,9 +5,9 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to tictactoe!");
         System.out.println("Please enter name of player 1:");
-        String p1Name = Input.scan.nextLine();
+        String p1Name = Input.getString();
         System.out.println("Please enter name of player 2:");
-        String p2Name = Input.scan.nextLine();
+        String p2Name = Input.getString();
 
         Game game = new Game(p1Name, p2Name);
         game.run();
